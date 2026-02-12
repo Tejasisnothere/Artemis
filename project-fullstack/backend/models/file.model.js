abf97ea553036d9daf9bf5707ea9683d66be26c1
tanishqbhanot <tanishqbhanotwrk@gmail.com>
2026-02-13 03:11:55 +0530

    Added exoskeletons of models.

---
+++ project-fullstack/backend/models/file.model.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema({
+
+});
+
+export default mongoose.model("File", fileSchema);
---
+++ project-fullstack/backend/models/review.model.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.model({
+
+});
+
+export default mongoose.model("Review", reviewSchema);
