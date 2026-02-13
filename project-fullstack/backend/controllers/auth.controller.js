b8134035f452a9c102bbde76290e6448e81c5c5e
tanishqbhanot <tanishqbhanotwrk@gmail.com>
2026-02-13 10:09:42 +0530

    Added part of the audio ingestion pipeline. Audio workers and compose service yet to be added.

---
+++ project-fullstack/backend/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+
+const registerUser = async () => {
+
+};
+
+const loginUser = async () => {
+
+};
+
+const logoutUser = async () => {
+
+};
+
+const updateUser = async () => {
+
+};
+
+const deleteUser = async () => {
+
+};
